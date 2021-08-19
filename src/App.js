ce31502bf81e31b1ba8536776292352e6f986971
demonicirfan <hashtag.irfan@gmail.com>
2021-08-19 11:50:18 +0530

    error boundaries

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import PortalDemo from "./Components.js/portals/PortalDemo";
-import FocusInput from "./Components.js/refs/FocusInput";
-import FRParentInput from "./Components.js/refs/FRParentInput";
-
+// import PortalDemo from "./Components.js/portals/PortalDemo";
+// import FocusInput from "./Components.js/refs/FocusInput";
+// import FRParentInput from "./Components.js/refs/FRParentInput";
 //import RefsDemo from "./Components.js/refs/RefsDemo";
-
 //import ParentComponent from "./Components.js/memo/ParentComponent";
+import ErrorBoundary from "./Components.js/errorBoundaries/ErrorBoundary";
+import Hero from "./Components.js/errorBoundaries/Hero";
 
 function App() {
   return (
     <div>
-      {/* <RefsDemo /> */}
-      <PortalDemo />
+      <ErrorBoundary>
+        <Hero heroName="Batman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Joker" />
+      </ErrorBoundary>
     </div>
   );
 }
